136af540188637c064c35ae10e737815c21b1f05
Jean Fernandes de Macedo <jfmacedo91@gmail.com>
2026-01-22 19:27:47 -0300

    create tables

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
-import { ProductsApi } from "./api/products/index.ts";
+import { AuthApi } from "./api/auth/index.ts";
+import { LmsApi } from "./api/lms/index.ts";
 import { Core } from "./core/core.ts";
 import { logger } from "./core/middlewares/logger.ts";
 
@@ -6,6 +7,7 @@ const core = new Core();
 
 core.router.use([logger]);
 
-new ProductsApi(core).init();
+new AuthApi(core).init();
+new LmsApi(core).init();
 
 core.init();
